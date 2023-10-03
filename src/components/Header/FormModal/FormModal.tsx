4f8e79b827738d6cdfc7a7d52a2965c4fefdeada
tommartinez21 <tommartinez@santandertecnologia.com.ar>
2023-10-03 00:20:21 -0300

    update

--- src/components/Header/FormModal/FormModal.tsx
+++ src/components/Header/FormModal/FormModal.tsx
@@ -7,39 +7,56 @@ import { BoxStyled, ModalStyled, TextFieldStyled, ButtonStyled, ButtonContainer 
 
 const FormModal: React.FC<FormModalProps> = ({ openModal, setOpenModal }) => {
 
-    const [ingredientsQuantity, setIngredientsQuantity] = useState<number>(1)
-
-    const tagList = [
+    const tagListDefault = [
         {
             label: "Title",
-            type: "text"
+            type: "text",
+            value: ""
         },
         {
             label: "URL",
-            type: "url"
+            type: "url",
+            value: ""
         },
         {
             label: "Image URL",
-            type: "url"
+            type: "url",
+            value: ""
         },
         {
             label: "Publisher",
-            type: "text"
+            type: "text",
+            value: ""
         },
         {
             label: "Preparation Time (in minutes)",
-            type: "number"
+            type: "number",
+            value: ""
         },
         {
             label: "Servings",
-            type: "number"
+            type: "number",
+            value: ""
         },
         {
             label: "Ingredients",
             type: "text",
-            quantity: ingredientsQuantity
+            value: [""]
         },
     ];
+
+    const [tagList, setTagList] = useState(tagListDefault)
+
+    const setTagValue = (indexTag: number, event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const isAnIngredient = Array.from(event.currentTarget.labels!)[0].textContent?.includes("Ingredient");
+        const tagListToBeSetted = [...tagList];
+        tagListToBeSetted.forEach((tag, index) => {
+            if (isAnIngredient && tag.label === "Ingredients" && Array.isArray(tag.value)) tag.value[indexTag] = event.currentTarget.value
+            else if (!isAnIngredient && index === indexTag) tag.value = event.currentTarget.value
+        })
+        setTagList(tagListToBeSetted)
+        console.log(tagList, tagListToBeSetted)
+    }
 
 
     return (
@@ -53,26 +70,30 @@ const FormModal: React.FC<FormModalProps> = ({ openModal, setOpenModal }) => {
             <BoxStyled component="form" autoComplete="off">
                 <Typography variant='h6'>RECIPE DATA</Typography>
                 {tagList.map((tag, index) => (
-                    tag.label !== "Ingredients" ? (
-                        < TextFieldStyled
-                            type={tag.type}
-                            label={tag.label}
-                            variant="filled"
-                            size="small"
-                            InputProps={{ inputProps: { min: 1 } }}
-                        />
-                    ) : ([
-                        <Typography variant='h6'>INGREDIENTS</Typography>,
-                        ...Array.from(Array(ingredientsQuantity).keys()).map(() => (
+                    tag.label === "Ingredients" && Array.isArray(tag.value) ? ([
+                        <Typography key={index} variant='h6'>INGREDIENTS</Typography>,
+                        ...tag.value.map((ingredient, index) => (
                             < TextFieldStyled
-                                type='ur'
-                                label=""
-                                id="filled-hidden-label-small"
-                                defaultValue="Small"
+                                type={tag.type}
+                                key={index}
+                                label={`Ingredient ${index + 1}`}
+                                onChange={(event) => setTagValue(index, event)}
+                                required
                                 variant="filled"
                                 size="small"
                             />
-                        ))])
+                        ))]) : (
+                        < TextFieldStyled
+                            type={tag.type}
+                            label={tag.label}
+                            onChange={(event) => setTagValue(index, event)}
+                            variant="filled"
+                            key={index}
+                            size="small"
+                            required
+                            InputProps={{ inputProps: { min: 1 } }}
+                        />
+                    )
                 ))}
                 <ButtonContainer><ButtonStyled variant="contained" startIcon={<CloudUpload />} onClick={() => null}>Add</ButtonStyled></ButtonContainer>
             </BoxStyled>
